e168d10ee9dbdaeefe6d8d1fe995c0b878e26742
Tomson0119 <kjs980119@gmail.com>
2020-11-18 17:35:35 +0900

    계속 점프 시도 중

--- 2DProject/SlashBit/player.py
+++ 2DProject/SlashBit/player.py
@@ -10,14 +10,14 @@ class Player:
     ANIM_INDEX = [1, 2, 8, 3, 4, 5]
 
     KEY_MAP = {
-        (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
-        (SDL_KEYDOWN, SDLK_RIGHT): ( 1,  0),
-        (SDL_KEYDOWN, SDLK_UP):    ( 0,  1),
-        (SDL_KEYDOWN, SDLK_DOWN):  ( 0, -1),
-        (SDL_KEYUP, SDLK_LEFT):    ( 1,  0),
-        (SDL_KEYUP, SDLK_RIGHT):   (-1,  0),
-        (SDL_KEYUP, SDLK_UP):      ( 0, -1),
-        (SDL_KEYUP, SDLK_DOWN):    ( 0,  1)
+        (SDL_KEYDOWN, SDLK_LEFT): (-1, 0),
+        (SDL_KEYDOWN, SDLK_RIGHT): (1, 0),
+        (SDL_KEYDOWN, SDLK_UP): (0, 1),
+        (SDL_KEYDOWN, SDLK_DOWN): (0, -1),
+        (SDL_KEYUP, SDLK_LEFT): (1, 0),
+        (SDL_KEYUP, SDLK_RIGHT): (-1, 0),
+        (SDL_KEYUP, SDLK_UP): (0, -1),
+        (SDL_KEYUP, SDLK_DOWN): (0, 1)
     }
 
     def __init__(self):
@@ -28,34 +28,54 @@ class Player:
         self.anim = 0
         self.left = False
         self.state = Player.IDLE
-        self.fall = False
+        self.jump_speed = 0
+
+    def update_state(self):
+        if self.state == Player.IDLE:
+            self.state = Player.WALK
+        elif self.state == Player.WALK:
+            self.state = Player.IDLE
+
 
     def update_delta(self, ddx, ddy):
         dx, dy = self.delta
         dx += ddx
-        dy += ddy
-
-        if dx != 0:
-            self.state = Player.WALK
-        elif dy < 0:
-            self.state = Player.CROUCH
-        elif dy > 0:
-            self.state = Player.JUMP
-        else:
-            self.state = Player.IDLE
-
+        self.update_state()
         self.delta = dx, dy
 
     def handle_event(self, e):
         pair = (e.type, e.key)
         if pair in Player.KEY_MAP:
             self.update_delta(*Player.KEY_MAP[pair])
+            print("Key is being pressed")
+        if pair == (SDL_KEYDOWN, SDLK_UP):
+            self.jump_speed = 500
+            self.state = Player.JUMP
 
     def update(self):
         x, y = self.pos
         dx, dy = self.delta
-        self.pos = x + dx * self.speed * gfw.delta_time, y + dy * self.speed * gfw.delta_time
-        self.anim = (self.anim + 1) % Player.ANIM_INDEX[self.state]
+
+        if self.state == Player.JUMP:
+            self.pos = point_add(self.pos, (x, self.jump_speed * gfw.delta_time))
+            self.jump_speed -= 500 * gfw.delta_time
+            if self.anim < Player.ANIM_INDEX[self.state] - 1:
+                self.anim = (self.anim + 1) % Player.ANIM_INDEX[self.state]
+
+            y = self.pos[1]
+            if y < 100:
+                y = 100
+                self.state = Player.IDLE
+
+        elif self.state == Player.WALK:
+            x += dx * self.speed * gfw.delta_time
+            self.anim = (self.anim + 1) % Player.ANIM_INDEX[self.state]
+
+        # This is going to be Crouch or attack or idle
+        else:
+            self.anim = (self.anim + 1) % Player.ANIM_INDEX[self.state]
+
+        self.pos = x, y
 
     def draw(self):
         width, height = 72, 72
