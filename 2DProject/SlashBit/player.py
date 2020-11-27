1ba6d9cf60243687d8be8f51f08444e96a18c343
Tomson0119 <kjs980119@gmail.com>
2020-11-27 21:43:35 +0900

    Falling state 생성 타일과의 충돌체크 구현

--- 2DProject/SlashBit/player.py
+++ 2DProject/SlashBit/player.py
@@ -19,7 +19,6 @@ class IdleState:
         self.player = None
         self.time = 0
         self.anim = 0
-        self.move_speed = 10
         self.left_pressed = False
         self.right_pressed = False
 
@@ -52,16 +51,20 @@ class IdleState:
         elif pair == (SDL_KEYDOWN, SDLK_z):
             self.player.set_state(AttackState)
 
+        key_state = SDL_GetKeyboardState(None)
+        if key_state[SDL_SCANCODE_LEFT]:
+            print("left")
+
     def update(self):
         self.time += gfw.delta_time
 
-        # Delta Error Fix\
+        # Delta Error Fix
         if self.player.delta[0] > 0 and not self.right_pressed:
             self.player.delta = point_add(self.player.delta, (-1, 0))
         elif self.player.delta[0] < 0 and not self.left_pressed:
             self.player.delta = point_add(self.player.delta, (1, 0))
 
-        move_obj(self.player, self.move_speed)
+        move_obj(self.player, self.player.move_speed)
 
         frame = self.time * 10
         if self.player.delta[0] != 0:
@@ -95,12 +98,13 @@ class JumpState:
         self.player = None
         self.time = 0
         self.anim = 0
-        self.jump_speed = 25
+        self.jump_speed = 15
 
     def enter(self):
         self.time = 0
         self.anim = 0
         self.player.delta = point_add(self.player.delta, (0, self.jump_speed))
+        move_obj(self.player, self.player.move_speed)
 
     def exit(self):
         pass
@@ -115,29 +119,10 @@ class JumpState:
             state.left_pressed = False
 
     def update(self):
-        self.time += gfw.delta_time
-
-        # Decrease delta y
-        x, y = self.player.pos
-        dx, dy = self.player.delta
-        if y >= 100:
-            dy -= 2
-        else:
-            y = 100
-            dy = 0
-            self.player.set_state(IdleState)
-
-        self.player.pos = x, y
-        self.player.delta = dx, dy
-
-        move_obj(self.player, 10)
-
-        frame = self.time * 20
-        if self.anim < 3:
-            self.anim = int(frame) % 4
+        pass
 
     def draw(self):
-        self.player.draw_ex(self.anim, 4)
+        pass
 
 
 class CrouchState:
@@ -274,16 +259,71 @@ class DeathState:
         self.player.draw_ex(self.anim, 5)
 
 
+class FallingState:
+    BB_RECT = (-31, -48, 35, 49)
+
+    @staticmethod
+    def get(player):
+        if not hasattr(FallingState, 'singleton'):
+            FallingState.singleton = FallingState()
+            FallingState.singleton.player = player
+        return FallingState.singleton
+
+    def __init__(self):
+        self.player = None
+        self.time = 0
+        self.anim = 3
+
+    def enter(self):
+        self.time = 0
+
+    def exit(self):
+        pass
+
+    def handle_event(self, e):
+        pair = (e.type, e.key)
+
+        state = IdleState.get(self.player)
+        if pair == (SDL_KEYUP, SDLK_RIGHT):
+            state.right_pressed = False
+        elif pair == (SDL_KEYUP, SDLK_LEFT):
+            state.left_pressed = False
+
+    def update(self):
+        self.time += gfw.delta_time
+
+        dx, dy = self.player.delta
+        dy -= 1
+        self.player.delta = dx, dy
+        move_obj(self.player, self.player.move_speed)
+
+        _, foot, _, _ = self.player.get_bb()
+        x, y = self.player.pos
+        if foot <= self.player.selected_t:
+            print(self.player.selected_t)
+            dy = 0
+            y = self.player.selected_t + (self.player.pos[1] - foot)
+            # y = self.player.selected_t
+            self.player.pos = x, y
+            self.player.delta = dx, dy
+            self.player.set_state(IdleState)
+
+    def draw(self):
+        self.player.draw_ex(self.anim, 4)
+
+
 class Player:
     def __init__(self):
         self.image = gfw.image.load(res(CH_DIR + 'soldier_animation_sheet.png'))
         self.state = None
         self.set_state(IdleState)
-        self.pos = 100, 100
+        self.pos = 100, 200
         self.delta = 0, 0
+        self.move_speed = 6
         self.size = 72
         self.isLeft = False
         self.life = 5
+        self.selected_t = 0
 
     def set_state(self, clazz):
         if self.state is not None:
@@ -302,6 +342,12 @@ class Player:
         if self.life == 0:
             self.life -= 1
             self.set_state(DeathState)
+
+        left, foot, right, _ = self.get_bb()
+        t = self.get_below_tile_top()
+        if foot > t:
+            self.selected_t = t
+            self.set_state(FallingState)
 
     def draw(self):
         self.state.draw()
@@ -325,3 +371,40 @@ class Player:
             r *= -1
         bb = x + l, y + b, x + r, y + t
         return bb
+
+    def get_below_tile_top(self):
+        selected = None
+        sel_top = 0
+        x, y = self.pos
+
+        for tile in gfw.world.objects_at(gfw.layer.tile):
+            l, b, r, t = tile.get_bb()
+            if l <= x <= r and y > t:
+                if selected is None:
+                    selected = tile
+                    sel_top = t
+                else:
+                    if t > sel_top:
+                        selected = tile
+                        sel_top = t
+        if selected is not None:
+            _, _, _, ret = selected.get_bb()
+        else:
+            ret = -100
+        return ret
+
+    def get_left_tile_right(self):
+        selected = None
+        sel_right = 0
+        x, y = self.pos
+
+        for tile in gfw.world.objects_at(gfw.layer.tile):
+            l, b, r, t = tile.get_bb()
+            if b <= y <= t and x > r :
+                selected = tile
+                sel_top = t
+        if selected is not None:
+            _, _, _, ret = selected.get_bb()
+        else:
+            ret = -100
+        return ret
