5b65a747d1e8c534c064e97ace25a56f78fd1613
Tomson0119 <kjs980119@gmail.com>
2020-12-03 13:17:47 +0900

    스테이지에 아이템 생성하도록 구현

--- 2DProject/SlashBit/stage_gen.py
+++ 2DProject/SlashBit/stage_gen.py
@@ -1,15 +1,16 @@
 import gfw
 from pico2d import *
-from tile import Tile
-import item
+from object import *
+import object
 import gobj
+from enemy import *
 
-UNIT_PER_LINE = 100
+UNIT_PER_LINE = 70
 SCREEN_LINES = 10
 BLOCK_SIZE = 96
 
 lines = []
-ASCII = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':',
+TILES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':',
          ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
 
 
@@ -20,7 +21,7 @@ def load(file):
         lines = ch
     current_x = 0
     map_index = 0
-    create_at = get_canvas_width() + 2 * BLOCK_SIZE
+    create_at = get_canvas_width() * 10 + 2 * BLOCK_SIZE
 
 
 def count():
@@ -31,6 +32,7 @@ def update(dx):
     global current_x, create_at
     current_x += dx
     ret = True
+
     while current_x < create_at:
         ret = create_column()
         if not ret:
@@ -44,6 +46,7 @@ def create_column():
     for row in range(SCREEN_LINES):
         ch = get(map_index, row)
         if ch == '#':
+            print("stage gen end")
             return False
         create_object(ch, current_x, y)
         y += BLOCK_SIZE
@@ -59,8 +62,31 @@ def get(x, y):
 
 
 def create_object(ch, x, y):
-    if ch in ASCII:
+    if ch in TILES:
         y -= BLOCK_SIZE // 2 + 48
         x -= BLOCK_SIZE // 2
         obj = Tile(ord(ch) - ord('0'), x, y)
         gfw.world.add(gfw.layer.tile, obj)
+    elif ch == 'h':
+        y -= BLOCK_SIZE // 2 + 32
+        x -= BLOCK_SIZE // 2 - 16
+        obj = Heart(x, y)
+        gfw.world.add(gfw.layer.item, obj)
+    elif ch == 'r':
+        y -= BLOCK_SIZE // 2 + 32
+        x -= BLOCK_SIZE // 2 - 16
+        obj = Potion('red', x, y)
+        gfw.world.add(gfw.layer.item, obj)
+    elif ch == 'b':
+        y -= BLOCK_SIZE // 2 + 32
+        x -= BLOCK_SIZE // 2 - 16
+        obj = Potion('blue', x, y)
+        gfw.world.add(gfw.layer.item, obj)
+    elif ch == 's':
+        y -= BLOCK_SIZE // 2 + 55
+        x -= BLOCK_SIZE // 2
+        obj = Spike(x, y)
+        gfw.world.add(gfw.layer.spike, obj)
+    elif ch == 'e':
+        obj = Goblin(x, y)
+        gfw.world.add(gfw.layer.enemy, obj)
